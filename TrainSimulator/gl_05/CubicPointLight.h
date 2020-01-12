1dfb7ddb2d457107542b00424e031767fa3d533e
paolo21d <paolo21d@gmail.com>
2020-01-12 19:19:14 +0100

    Fixed CubicLamp colour

--- TrainSimulator/gl_05/CubicPointLight.h
+++ TrainSimulator/gl_05/CubicPointLight.h
@@ -9,11 +9,12 @@ public:
 	Cube* cube;
 
 	CubicPointLight(ShaderProgram* _shader, glm::vec3 _lightPos,
-		glm::vec3 _cubeSize, const char* _cubeFileName, ShaderProgram* _cubeShader,
-		glm::vec3 _ambient = { 0.2f, 0.2f, 0.2f }, glm::vec3 _diffuse = { 0.5f, 0.5f, 0.5f }, glm::vec3 _specular = { 1.0f, 1.0f, 1.0f },
+		ShaderProgram* _cubeShader, glm::vec3 _diffuse = { 0.5f, 0.5f, 0.5f }, glm::vec3 _cubeSize = { 0.25f, 0.25f, 0.25f },
+		glm::vec3 _ambient = { 0.2f, 0.2f, 0.2f }, glm::vec3 _specular = { 1.0f, 1.0f, 1.0f },
 		float _constant = 1.0f, float _linear = 0.09f, float _quadratic = 0.032f):
 		PointLight(_shader, _lightPos, _ambient, _diffuse, _specular, _constant, _linear, _quadratic)	{
-		cube = new Cube(_lightPos, _cubeSize, _cubeFileName, _cubeShader);
+		cube = new Cube(_lightPos, _cubeSize, "iipw.png", _cubeShader);
+	
 	}
 
 	~CubicPointLight() {
@@ -21,6 +22,8 @@ public:
 	}
 
 	void draw() {
+		cube->theProgram->Use();
+		cube->theProgram->setVec3("colour", diffuse);
 		cube->draw();
 	}
 
