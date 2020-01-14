1dacf25c8eda5154f531d3611bd6d9e8f5b58937
paolo21d <paolo21d@gmail.com>
2020-01-14 10:55:12 +0100

    Improved code of CubicPointLight

--- TrainSimulator/gl_05/CubicPointLight.h
+++ TrainSimulator/gl_05/CubicPointLight.h
@@ -1,47 +1,15 @@
 #pragma once
-#include "Object.h"
-#include "PointLight.h"
-#include "Cube.h"
+#include "ShapedPointLight.h"
 class CubicPointLight : 
-	public PointLight, 
-	public Object {
+	public ShapedPointLight{
 public:
 	Cube* cube;
 
 	CubicPointLight(ShaderProgram* _shader, glm::vec3 _lightPos,
-		ShaderProgram* _cubeShader, glm::vec3 _diffuse = { 0.5f, 0.5f, 0.5f }, glm::vec3 _cubeSize = { 0.25f, 0.25f, 0.25f },
+		ShaderProgram* _blockShader, glm::vec3 _diffuse = { 0.5f, 0.5f, 0.5f }, glm::vec3 _blockSize = { 0.25f, 0.25f, 0.25f },
 		glm::vec3 _ambient = { 0.2f, 0.2f, 0.2f }, glm::vec3 _specular = { 1.0f, 1.0f, 1.0f },
-		float _constant = 1.0f, float _linear = 0.09f, float _quadratic = 0.032f):
-		PointLight(_shader, _lightPos, _ambient, _diffuse, _specular, _constant, _linear, _quadratic)	{
-		cube = new Cube(_lightPos, _cubeSize, "iipw.png", _cubeShader);
-	
+		float _constant = 1.0f, float _linear = 0.09f, float _quadratic = 0.032f) :
+		ShapedPointLight(_shader, _lightPos, _diffuse, _ambient, _specular, _constant, _linear, _quadratic) {
+		block = new Cube(_lightPos, _blockSize, "iipw.png", _blockShader);
 	}
-
-	~CubicPointLight() {
-		delete cube;
-	}
-
-	void draw() {
-		cube->theProgram->Use();
-		cube->theProgram->setVec3("colour", diffuse);
-		cube->draw();
-	}
-
-	void move(glm::vec3 vector) {
-		PointLight::move(vector);
-		cube->move(vector);
-	}
-
-	void scale(glm::vec3 vector) {
-		cube->scale(vector);
-	}
-
-	void rotate(glm::vec3 vector) {
-		cube->rotate(vector);
-	}
-
-	void changeColour(glm::vec3 vector) {
-		PointLight::changeColour(vector);
-	}
-
 };
