06128f33de535a6811914086cbec60fe8b9c797f
paolo21d <paolo21d@gmail.com>
2020-01-14 11:19:23 +0100

    Added CylindricalPointLight

--- TrainSimulator/gl_05/main.cpp
+++ TrainSimulator/gl_05/main.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #include "Tracks.h"
 #include "Sphere.h"
 #include "SphericalPointLight.h"
+#include "CylindricalPointLight.h"
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 const float MAX_FPS = 60.0f;
@@ -122,6 +123,7 @@ int main()
 		CubicPointLight cubicLamp2(&ourShader, glm::vec3(1.0f, 2.0f, 1.0f), &lampShader, glm::vec3(0.0f, 0.0f, 1.0f));
 		CubicPointLight cubicLamp3(&ourShader, glm::vec3(-1.0f, 2.0f, 1.0f), &lampShader, glm::vec3(0.0f, 1.0f, 0.0f));
 		SphericalPointLight sphericalLamp(&ourShader, glm::vec3(-4.0f, 2.0f, 1.0f), &lampShader, glm::vec3(1.0f, 0.0f, 1.0f));
+		CylindricalPointLight cylindricalLamp(&ourShader, glm::vec3(-5.0f, 2.0f, 1.0f), &lampShader, glm::vec3(0.0f, 0.0f, 1.0f));
 
 		GLfloat rot_angle = 0.75f;
 		ourShader.Use();
@@ -198,6 +200,7 @@ int main()
 			cubicLamp3.draw();
 
 			sphericalLamp.draw();
+			cylindricalLamp.draw();
 
 			kostka1.rotate(glm::vec3(rot_angle, 0.0f, 0.0f));
 			kostka2.rotate(glm::vec3(rot_angle, 0.0f, 0.0f));
