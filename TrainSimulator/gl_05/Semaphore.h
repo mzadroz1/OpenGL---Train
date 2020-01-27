00cd646c3c73cf5f2e37cc3827ab144eced925ce
tomica28 <01133399@pw.edu.pl>
2020-01-27 13:04:03 +0100

    Semaphore update

--- TrainSimulator/gl_05/Semaphore.h
+++ TrainSimulator/gl_05/Semaphore.h
@@ -8,8 +8,6 @@ class Semaphore :
 	public Composite
 {
 private:
-
-
 public:
 	Semaphore(glm::vec3 coordinates, ShaderProgram* shader, ShaderProgram* lampShader)
 	{
@@ -17,29 +15,25 @@ public:
 		items.push_back(stick);
 		Object* box = new Cube(coordinates + glm::vec3(0.0f, 4.8f, 0.0f), { 0.5f,1.6f,0.5f }, "steel.png", shader);
 		items.push_back(box);
-		Object* hat1 = new Sphere(coordinates + glm::vec3(0.f, 4.5f, 0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
+		Object* hat1 = new Sphere(coordinates + glm::vec3(0.f, 4.5f, -0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
 		items.push_back(hat1);
-		Object* hat2 = new Sphere(coordinates + glm::vec3(0.f, 5.f, 0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
+		Object* hat2 = new Sphere(coordinates + glm::vec3(0.f, 5.f, -0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
 		items.push_back(hat2);
-		Object* hat3 = new Sphere(coordinates + glm::vec3(0.f, 5.5f, 0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
+		Object* hat3 = new Sphere(coordinates + glm::vec3(0.f, 5.5f, -0.25f), glm::vec3(0.25f, 0.05f, 0.4f), "moon1024.bmp", shader);
 		items.push_back(hat3);
-
-		SphericalPointLight* lampGreen = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 4.25f, 0.25f), lampShader, {0.3f, 0.3f, 0.3f}, glm::vec3(0.15f, 0.15f, 0.15f));
-		SphericalPointLight* lampYellow = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 4.75f, 0.25f), lampShader, { 0.3f, 0.3f, 0.3f }, glm::vec3(0.15f, 0.15f, 0.15f));
-		SphericalPointLight* lampRed = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 5.25f, 0.25f), lampShader, { 1.f, 0.f, 0.f }, glm::vec3(0.15f, 0.15f, 0.15f));
-
+		SphericalPointLight* lampGreen = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 4.25f, -0.25f), lampShader, { 0.3f, 0.3f, 0.3f }, glm::vec3(0.15f, 0.15f, 0.15f));
+		SphericalPointLight* lampYellow = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 4.75f, -0.25f), lampShader, { 0.3f, 0.3f, 0.3f }, glm::vec3(0.15f, 0.15f, 0.15f));
+		SphericalPointLight* lampRed = new SphericalPointLight(shader, coordinates + glm::vec3(0.f, 5.25f, -0.25f), lampShader, { 1.f, 0.f, 0.f }, glm::vec3(0.15f, 0.15f, 0.15f));
 		items.push_back(lampRed);
 		items.push_back(lampYellow);
 		items.push_back(lampGreen);
-		
-	}
 
+	}
 	void changeToGreen() {
-		((SphericalPointLight *)items[5])->changeColour({ 0.3f, 0.3f, 0.3f });
+		((SphericalPointLight*)items[5])->changeColour({ 0.3f, 0.3f, 0.3f });
 		((SphericalPointLight*)items[6])->changeColour({ 0.3f, 0.3f, 0.3f });
 		((SphericalPointLight*)items[7])->changeColour({ 0.f, 1.f, 0.f });
 	}
-
 	void changeToRed() {
 		((SphericalPointLight*)items[5])->changeColour({ 1.f, 0.f, 0.f });
 		((SphericalPointLight*)items[6])->changeColour({ 0.3f, 0.3f, 0.3f });
